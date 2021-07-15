b963b1d6763cc31977f418dee2d25d968547557a
George Loera <gloera8325@gmail.com>
2021-07-15 12:31:22 -0500

    created a simple form for adding new members

--- src/Components/Roster/roster.js
+++ src/Components/Roster/roster.js
@@ -24,21 +24,79 @@ const Roster = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {contacts.map(() => (
+                    {contacts.map((contact) => (
                         <tr>
-                            <td> Brady </td>
-                            <td> Member </td>
-                            <td> 316 Madison 2550 </td>
-                            <td> Same </td>
-                            <td> Huntsville </td>
-                            <td> AR </td>
-                            <td> 72740 </td>
-                            <td> 479-981-6638 </td>
-                            <td> 479-981-6638</td>
+                            <td>{contact.member}</td>
+                            <td>{contact.status}</td>
+                            <td>{contact.mailingAddress}</td>
+                            <td>{contact.physicalAddress}</td>
+                            <td>{contact.city}</td>
+                            <td>{contact.state}</td>
+                            <td>{contact.zipcode}</td>
+                            <td>{contact.cellPhone}</td>
+                            <td>{contact.homePhone}</td>
                         </tr>                    
                     ))}               
                 </tbody>
             </table>
+            <h2> Add Member</h2>
+            <form>
+                <input 
+                type='text' 
+                name="Member Name (LN, FN)"
+                required="required"
+                placeholder="Enter Members Name"
+                />
+                <input 
+                type='text' 
+                name="Status"
+                required="required"
+                placeholder="Enter Current Status"
+                />
+                <input 
+                type='text' 
+                name="Mailing Address"
+                required="required"
+                placeholder="Enter Mailing Address"
+                />
+                <input 
+                type='text' 
+                name="Physical Address"
+                required="required"
+                placeholder="Enter Physical Address"
+                />
+                <input 
+                type='text' 
+                name="City"
+                required="required"
+                placeholder="Enter City"
+                />
+                <input 
+                type='text' 
+                name="State"
+                required="required"
+                placeholder="Enter State"
+                />
+                <input 
+                type='text' 
+                name="Zipcode"
+                required="required"
+                placeholder="Enter Zipcode"
+                />
+                <input 
+                type='text' 
+                name="Cell Phone"
+                required="required"
+                placeholder="Enter Cell Phone"
+                />
+                <input 
+                type='text' 
+                name="Home Phone"
+                required="required"
+                placeholder="Enter Home Phone"
+                />
+
+            </form>
         </div>
     )
 }
